3f61c1012051d655db1e97f8d87930c50f57d96a
asioting <asioting@gmail.com>
2019-03-14 13:46:15 +0800

    Initial version.

---
+++ message-service/src/main/java/com/example/controller/MessageServiceController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/message-service")
+public class MessageServiceController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+
+	@GetMapping("/msgs")
+	public String find() {
+		return "Hello World!";
+	}
+
+}
