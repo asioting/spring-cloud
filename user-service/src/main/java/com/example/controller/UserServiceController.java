af5c9cee15693a0599798731a0dda1ad4feafeeb
asioting <asioting@gmail.com>
2019-04-04 06:23:11 +0800

    User service. Initial version.

---
+++ user-service/src/main/java/com/example/controller/UserServiceController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/user-service")
+public class UserServiceController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+
+	@GetMapping("/users")
+	public String find() {
+		return "Users found!";
+	}
+
+}
