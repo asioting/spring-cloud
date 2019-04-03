b9f4cb6e46f9f6db95b9b6722fda7f596c18222a
asioting <asioting@gmail.com>
2019-04-04 06:20:41 +0800

    Updated to use Config Server.

--- message-service/src/main/java/com/example/controller/MessageServiceController.java
+++ message-service/src/main/java/com/example/controller/MessageServiceController.java
@@ -6,8 +6,10 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  *
  */
+@RefreshScope
 @RestController
 @RequestMapping("/message-service")
 public class MessageServiceController {
@@ -23,14 +26,17 @@ public class MessageServiceController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
+	@Value("${welcome.message:Hello}")
+	private String message;
+	
 	@GetMapping("/service-instances/{applicationName}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
 		return this.discoveryClient.getInstances(applicationName);
 	}
 
-	@GetMapping("/msgs")
+	@GetMapping("/messages")
 	public String find() {
-		return "Hello World!";
+		return message;
 	}
 
 }
